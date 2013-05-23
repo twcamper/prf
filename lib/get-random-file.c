e5fb92fe250987ea1be93746a6e2a9bca9a4ea9f
Tim Camper <twcamper@thoughtworks.com>
2013-05-23 00:01:38 -0500

    get_random_file uses new entry list type

--- lib/get-random-file.c
+++ lib/get-random-file.c
@@ -10,63 +10,23 @@ static bool can_play(char *ext[], size_t n, char *filename)
 
   return false;
 }
-static bool already_tried(size_t index, size_t tried_list[], size_t n)
+static char *find_file(FilteredEntryList entries, char **ext, size_t extension_count)
 {
-  for (size_t i = 0; i < n; i++)
-    if (tried_list[i] == index)
-      return true;
-
-  return false;
-}
-static char *find_file(glob_t entries, char **ext, size_t extension_count)
-{
-  size_t tried[BUFSIZ], tried_count = 0;
-  size_t entry;
-  char dir_glob[PATH_MAX + 2];
-  static char path[FILENAME_MAX + 1];
-  struct stat fs;
-
-  while (tried_count < entries.gl_pathc) {
-    /* pick random entry */
-    entry = rand() % entries.gl_pathc;
-
-    /* keep trying if this one already failed */
-    if (already_tried(entry, tried, tried_count))
-      continue;
-
-    /* store in list of those tried */
-    tried[tried_count++] = entry;
-
-    /* stat the file we just found */
-    if (stat(entries.gl_pathv[entry], &fs) != 0) {
-      perror(entries.gl_pathv[entry]);
-      exit(EXIT_FAILURE);
-    }
+  ListItem *entry;
+  while ((entry = random_item(entries))) {
 
     /* get new set of entries if it's another directory */
-    if (S_ISDIR(fs.st_mode)) {
-      strcpy(dir_glob, entries.gl_pathv[entry]);
-      strncat(dir_glob, "*", 1);
-      globfree(&entries);
-      errno = 0;
-      if ((glob(dir_glob, GLOB_MARK|GLOB_ERR, NULL, &entries) != 0
-           || (&entries && entries.gl_pathc == 0))) {
-        /* not getting GLOB_NOMATCH, so resort to testing count of paths found */
-        fprintf(stderr, "%s:%d glob: %s '%s'\n", __FILE__, __LINE__, strerror(errno), dir_glob);
+    if (entry->is_dir) {
+      if (!(entries = filtered_entry_list(entry->file_name, ext))) {
+        fprintf(stderr, "%s:%d %s '%s'\n", __FILE__, __LINE__, "Can't make entry list",  entry->file_name);
         exit(EXIT_FAILURE);
       }
 
       /* RECURSION */
       return find_file(entries, ext, extension_count);
     }
-    /* we're done if it's a regular file with a good extension */
-    if (S_ISREG(fs.st_mode)) {
-      if (can_play(ext, extension_count, entries.gl_pathv[entry])) {
-        strcpy(path, entries.gl_pathv[entry]);
-        globfree(&entries);
-        return path;
-      }
-    }
+    /* we're done: it's a regular file with a good extension */
+    return entry->file_name;
   }
   return NULL;
 }
