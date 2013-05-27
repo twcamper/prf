60d9e151c871a408d896578ee07263fca2f55727
Tim Camper <twcamper@thoughtworks.com>
2013-05-26 19:12:32 -0500

    create dir path and write to log file: open for append fails when we've just created the dir, but works on subsequent calls

--- include/played.h
+++ include/played.h
@@ -4,8 +4,10 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 int log_as_played(char *, char *);
 #endif
--- lib/played.c
+++ lib/played.c
@@ -2,6 +2,8 @@
 
 /* private function prototype declarations */
 static int expand_home_dir(char *);
+static FILE *get_output_stream(char *);
+static int make_dir_p(char *);
 
 int log_as_played(char *file_name, char *log_file)
 {
@@ -28,10 +30,24 @@ int log_as_played(char *file_name, char *log_file)
   if (expand_home_dir(log_file) != 0)
     return -1;
 
-  struct stat fs;
-  if (stat(log_file, &fs) != 0)
-    perror(log_file);
+  FILE *ostream;
+  if ((ostream = get_output_stream(log_file)) == NULL)
+    return -1;
 
+  size_t output_len = strlen(file_name);
+  bool output_error = false;
+
+  if ((size_t)fprintf(ostream, "%s\n", file_name) < output_len) {
+    fprintf(stderr, "%s:%d Error writing to %s\n", __FILE__, __LINE__,  log_file);
+    output_error = true;
+  }
+  if (fclose(ostream) == EOF) {
+    fprintf(stderr, "%s:%d Error closeing %s\n", __FILE__, __LINE__,  log_file);
+    output_error = true;
+  }
+  if (output_error) {
+    return -1;
+  }
   return 0;
 }
 static int expand_home_dir(char *filename)
@@ -48,10 +64,58 @@ static int expand_home_dir(char *filename)
       return -1;
     }
     /* shift string down the array to accommadate home path */
-    memmove(filename + home_len, tilde + 1, home_len);
+    memmove(filename + home_len, tilde + 1, strlen(tilde + 1));
     /* copy in home path */
     strncpy(filename, home, home_len);
   }
 
+  return 0;
+}
+static FILE *get_output_stream(char *filename)
+{
+  /* convert temporarily to the dir path */
+  char *final_separator = strrchr(filename, '/');
+  *final_separator = '\0';
+
+  struct stat fs;
+  errno = 0;
+  if (stat(filename, &fs) != 0) {
+    if (errno == ENOENT) {  /* we must create the dir(s) */
+      if (make_dir_p(filename) != 0)
+        return NULL;
+    } else {
+      perror(filename);
+      return NULL;
+    }
+  }
+  /* revert back to full file path */
+  *final_separator = '/';
+
+  /* open for appending */
+  FILE *fp;
+  if (!(fp = fopen(filename, "a"))) {
+    fprintf(stderr, "%s:%d Error opening %s\n", __FILE__, __LINE__,  filename);
+    return NULL;
+  }
+
+  return fp;
+}
+static int make_dir_p(char *dir_path)
+{
+  pid_t pid;
+
+  errno = 0;
+  if ((pid = fork()) == -1) {
+    perror("Can't fork to shell out to mkdir");
+    return -1;
+  }
+
+  if (pid == 0) {
+    errno = 0;
+    if (execlp("mkdir", "mkdir", "-p", dir_path, NULL) != 0) {
+      perror("mkdir -p");
+      return -1;
+    }
+  }
   return 0;
 }
